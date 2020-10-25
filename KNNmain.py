16a196706e2a05c86817b7d02a85423924a53e1e
jhreiser <jordanreiser1255@gmail.com>
2020-10-25 16:53:05 -0400

    Transferred in all data into readable numerical values and setup up the algorithm for learning.

--- KNNmain.py
+++ KNNmain.py
@@ -7,3 +7,22 @@ import numpy as np
 
 data = pd.read_csv("car.data")
 print(data.head())
+
+#Takes the labels and initializes them in to relative computable integers
+le = preprocessing.LabelEncoder()
+buying = le.fit_transform(list(data["buying"]))
+maint = le.fit_transform(list(data["maint"]))
+doors = le.fit_transform(list(data["doors"]))
+persons = le.fit_transform(list(data["persons"]))
+lug_boot = le.fit_transform(list(data["lug_boot"]))
+safety = le.fit_transform(list(data["safety"]))
+clss = le.fit_transform(list(data["class"]))
+
+predict = "class"
+
+#Converts all above data in to a single list for the Label then class is the Feature
+x = list(zip(buying, maint, doors, persons, lug_boot, safety))
+y = list(clss)
+
+#training and testing the model, same as linear regression
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
